8ace21e5c203d6c179d4c97d23f06b847ff88f84
Marleidis <marleidish@uninorte.edu.com.co>
2021-10-18 12:26:40 -0500

    Se actualiza archivo controlador de la bd

---
+++ bd.py
@@ -0,0 +1,58 @@
+import sqlite3
+from sqlite3 import Error
+
+def conectar():
+    try: 
+        conn = sqlite3.connect('db/datos.db')
+        return conn
+    except Error as err:
+        print("Ocurrio un error al establecer la conexión: " + str(err))
+        return None
+
+
+def ejecutar_insert(_sql, lista_parametros):
+    try:
+        conn = conectar()
+        if conn:
+            objeto_cursor = conn.cursor()
+            filas = objeto_cursor.execute(_sql, lista_parametros).rowcount
+            objeto_cursor.close
+            conn.commit()
+            conn.close()
+            return filas
+        else:
+            print("No se pudo ver establecer la conexión.  Ver errores previos.")
+            return -1
+    except Error as err:
+        print("Ocurrió un error al intentar ejecutar el comando: " + _sql + " " + str(err))
+        return -1
+
+
+def ejecutar_select(_sql, lista_parametros):
+    try:
+        conn = conectar()
+        if conn:
+            conn.row_factory = fabrica_diccionarios
+            objeto_cursor = conn.cursor()
+            if lista_parametros:
+                objeto_cursor.execute(_sql, lista_parametros)
+            else:
+                objeto_cursor.execute(_sql)
+            filas = objeto_cursor.fetchall()
+            objeto_cursor.close()
+            conn.close()
+            return filas
+        else:
+            print("No se puedo establecer la conexión. Ver errores previos.")
+            return None
+    except Error as err:
+        print("Ocurrió un error al intentar ejecutar el comando: " + _sql + " " + str(err))
+        return None 
+
+
+def fabrica_diccionarios(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
