6c8529caa73f9b68cf59ebec9d4bfdbce6695dc7
Esteban Marentes <90976215+EstebanMHMisionTic@users.noreply.github.com>
2021-10-26 13:24:04 -0500

    Comentar la línea que generaba problemas en pythonanywhere

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import re
-from validate_email import validate_email
+#from validate_email import validate_email
 
 pass_reguex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.])[A-Za-z\d@$!%*?&.]{8,}$"
 user_reguex = "^[a-zA-Z0-9_.-]+$"
